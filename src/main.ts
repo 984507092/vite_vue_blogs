e3c9652f4a38207a4862e546a80d98a4d7fdc974
heming <984507092@qq.com>
2024-03-13 12:12:38 +0800

    feat:新增文件配置

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,20 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+//pinia
+import { createPinia } from 'pinia'
+const pinia = createPinia()
+
+import ElementPlus from "element-plus";
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+import "element-plus/dist/index.css";
 
 const app = createApp(App);
+
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+
 app.use(router);
+app.use(pinia)
 app.mount("#app");
